75a4c6c82e5e203b80873acd56c93aeb686a29be
Alecsandru Dinu <dinu@alecsandru.ro>
2025-06-03 16:52:32 +0300

    added missing assets

---
+++ _assets/c352ebd15c84d5e4.js
@@ -0,0 +1,8 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([[22104],{
+
+/***/ 719098:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(813110);self._7ccc7343da2ae983607b3819c7bc1f93 = self._7ccc7343da2ae983607b3819c7bc1f93 || {};(function(__c) {__c.QHb=__c.M(()=>({}));
+}).call(self, self._7ccc7343da2ae983607b3819c7bc1f93);}
+
+}])
+//# sourceMappingURL=sourcemaps/c352ebd15c84d5e4.js.map
